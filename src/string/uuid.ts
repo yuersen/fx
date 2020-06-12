431df83b1fce9fd98a53a54d24de903a454e7ebb
yuersen <pxyamos@163.com>
2020-06-12 16:44:35 +0800

    feat: Add UUID

---
+++ src/string/uuid.ts
@@ -0,0 +1,23 @@
+/**
+ * 生成 UUID
+ * GUID是一种由算法生成的二进制长度为128位的数字标识符。
+ * GUID 的格式为“xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx”，其中的 x 是 0-9 或 a-f 范围内的一个32位十六进制数。
+ * 在理想情况下，任何计算机和计算机集群都不会生成两个相同的GUID。
+ * GUID 的总数达到了2^128（3.4×10^38）个，所以随机生成两个相同GUID的可能性非常小，但并不为0。GUID一词有时也专指微软对UUID标准的实现。
+ *
+ * @function
+ * @memberof Fx
+ * @returns {string}
+ * @example
+ * 
+ * // -> 0aef0c57-804c-400a-9387-c15c70c13b17
+ * Fx.uuid();
+ */
+ 
+ const uuid = function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
+export default uuid;
