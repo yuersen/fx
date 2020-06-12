7ff17787cc906f27d128e250d10085b3ebcbe290
yuersen <pxyamos@163.com>
2020-06-12 16:45:27 +0800

    Update uuid.ts

--- src/string/uuid.ts
+++ src/string/uuid.ts
@@ -4,6 +4,7 @@
  * GUID 的格式为“xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx”，其中的 x 是 0-9 或 a-f 范围内的一个32位十六进制数。
  * 在理想情况下，任何计算机和计算机集群都不会生成两个相同的GUID。
  * GUID 的总数达到了2^128（3.4×10^38）个，所以随机生成两个相同GUID的可能性非常小，但并不为0。GUID一词有时也专指微软对UUID标准的实现。
+ * @see https://stackoverflow.com/questions/105034/how-to-create-guid-uuid/2117523#2117523
  *
  * @function
  * @memberof Fx
